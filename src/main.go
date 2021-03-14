1b853870ff5b2c78ca78811d026351edcce128a6
Alex Gravenor <blazingkin@gmail.com>
2021-03-14 00:09:53 -0800

    Basic program flow

--- src/main.go
+++ src/main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"runtime"
+	"io/ioutil"
+	"os"
+	"encoding/json"
 )
 type HostInfo struct {
 	KernelFamily string
 }
 
 type Environment struct {
-	host HostInfo
+	Host 			HostInfo
+	Targets			[]string
+}
+
+type DatabaseEntry struct {
+	TargetName 			   string
+	InstallationCommands   map[string][]string
+}
+
+type Database struct {
+	Entries []DatabaseEntry
 }
 
 func main() {
 	// Load config
 	environment := probeEnvironment()
 
-	fmt.Println(environment.host.KernelFamily)
+	fmt.Println(environment.Host.KernelFamily)
+	fmt.Print(environment.Targets)
 	// Parse local database to find commands
+	generateCommandList(environment)
 
 	// Run them
 
@@ -25,10 +41,37 @@ func main() {
 
 func probeEnvironment() Environment {
 	result := Environment{}
-	result.host.KernelFamily = runtime.GOOS
+	result.Host.KernelFamily = runtime.GOOS
+
+	flag.Parse()
+	result.Targets = flag.Args()
 	return result
 }
 
-func determineDependencyResolution() {
+func generateCommandList(environment Environment) {
+	RawDBData, err := ioutil.ReadFile("gimmedb.json")
+	db := Database{}
+	AssertNo(err)
+	err = json.Unmarshal(RawDBData, &db)
+	AssertNo(err)
+	for _, Target := range environment.Targets {
+		fmt.Println(Target)
+		var satisfied bool = false
+		for _, DBEntry := range db.Entries {
+			if DBEntry.TargetName == Target {
+				satisfied = true
+			}
+		}
+		if !satisfied {
+			fmt.Printf("Could not figure out how to install %s", Target)
+			os.Exit(1)
+		}
+	}
+}
 
+func AssertNo(err error) {
+	if err != nil {
+		fmt.Printf("Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
